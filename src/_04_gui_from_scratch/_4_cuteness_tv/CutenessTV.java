87c63042f74f6f20340f37700f11e8e5bb881ba2
student <student@ip-172-31-63-152.us-west-2.compute.internal>
2023-03-04 14:24:12 -0800

    CUTENESS! (If videos would only work...)

---
+++ src/_04_gui_from_scratch/_4_cuteness_tv/CutenessTV.java
@@ -0,0 +1,95 @@
+package _04_gui_from_scratch._4_cuteness_tv;
+
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URI;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class CutenessTV implements ActionListener{
+//Inetiation for frames and stuff
+	JFrame screen = new JFrame();
+	JPanel remote = new JPanel();
+	JButton duckB = new JButton();
+	JButton frogB = new JButton();
+	JButton uniB = new JButton();
+	
+//Veriables
+	Dimension BIG = new Dimension(400, 400);
+	Dimension SMALL = new Dimension(200, 50);
+	
+	public void showRemote() {
+	//Frames
+		screen.setVisible(true);
+		screen.add(remote);
+		
+		screen.pack();
+		
+	//Buttons
+		duckB.addActionListener(this);
+		frogB.addActionListener(this);
+		uniB.addActionListener(this);
+		
+		remote.add(duckB);
+		remote.add(frogB);
+		remote.add(uniB);
+		
+		duckB.setPreferredSize(SMALL);
+		frogB.setPreferredSize(SMALL);
+		uniB.setPreferredSize(SMALL);
+		
+		//Text
+		duckB.setText("Avien Chanel");
+		frogB.setText("Simiaquadic Chanel");
+		uniB.setText("Fanticy Chanel");
+		
+		screen.pack();
+	}
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		JButton buttonPressed = (JButton) arg0.getSource();
+		
+		if(buttonPressed == duckB ) {
+			showDucks();
+		}
+		else if(buttonPressed == frogB) {
+			showFrog();
+		}
+		else if(buttonPressed == uniB) {
+			showFluffyUnicorns();
+		}
+	}
+//The Videos
+	void showDucks() {
+	     playVideo("https://www.youtube.com/watch?v=MtN1YnoL46Q");
+	}
+
+	void showFrog() {
+	     playVideo("https://www.youtube.com/watch?v=cBkWhkAZ9ds");
+	}
+
+	void showFluffyUnicorns() {
+	     playVideo("https://www.youtube.com/watch?v=a-xWhG4UU_Y");
+	}
+
+	void playVideo(String videoID) {
+	     
+	     // Workaround for Linux because "Desktop.getDesktop().browse()" doesn't 
+	     //work on some Linux implementations 
+	     try {
+	     if (System.getProperty("os.name").toLowerCase().contains("linux")) {
+	     if (Runtime.getRuntime().exec(new String[] { 
+	      "which", "xdg- open" }).getInputStream().read() != -1) {
+	     Runtime.getRuntime().exec(new String[] { "xdg-open", videoID }); 
+	     }
+	     } else {
+	          URI uri = new URI(videoID);
+	          java.awt.Desktop.getDesktop().browse(uri);
+	     }      } catch (Exception e) {
+	          e.printStackTrace();
+	     }
+	}
+}
